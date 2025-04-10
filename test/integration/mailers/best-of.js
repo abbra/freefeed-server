b8eb7859408172500c93089a4331622e71be3af5
David Mzareulyan <david@hiero.ru>
2025-04-10 13:12:46 +0300

    Explicitly use the 'v2' API for the for Best Of digest data fetching
    The React components we use in emails expects the v2 data shape

--- test/integration/mailers/best-of.js
+++ test/integration/mailers/best-of.js
@@ -1,4 +1,9 @@
 /* eslint-env node, mocha */
+import { readFile, writeFile } from 'fs/promises';
+import { join, resolve } from 'path';
+import { tmpdir } from 'os';
+
+import { v4 as createUuid } from 'uuid';
 import config from 'config';
 import expect from 'unexpected';
 import { render as renderEJS } from 'ejs';
@@ -10,13 +15,47 @@ import {
   sendWeeklyBestOfEmail,
 } from '../../../app/mailers/BestOfDigestMailer';
 import { addMailListener } from '../../../lib/mailer';
+import { createUser } from '../helpers/users';
+import { createPost } from '../helpers/posts-and-comments';
+import { testFiles } from '../models/attachment-data';
+import { Attachment, dbAdapter } from '../../../app/models';
+import cleanDB from '../../dbCleaner';
+import { getSummary } from '../../../app/support/BestOfDigest';
 
 describe('BestOfDigests', () => {
+  let luna, post, att;
+
+  before(async () => {
+    await cleanDB(dbAdapter.database);
+    luna = await createUser('luna');
+    post = await createPost(luna, 'Post body');
+
+    // Add attachment to post
+    {
+      const { name: fileName } = testFiles.medium;
+      const fixturesDir = resolve(__dirname, '../../fixtures');
+      const fileData = await readFile(resolve(fixturesDir, fileName));
+      const tmpPath = join(tmpdir(), `upl-${createUuid()}`);
+      await writeFile(tmpPath, fileData);
+      att = await Attachment.create(tmpPath, fileName, luna, post.id);
+    }
+  });
+
   describe('renderSummaryBody', () => {
     it(`should render a summary body and doesn't blow up`, async () => {
-      const body = await renderSummaryBody({ posts: [] });
+      const dailySummary = await getSummary(luna, 1);
+      const body = await renderSummaryBody(dailySummary);
       expect(body, 'to contain', '<style>');
-      expect(body, 'to contain', '<div class="posts"></div>');
+      expect(
+        body,
+        'to contain',
+        '<div class="posts"><div class="post timeline-post" data-author="luna"',
+      );
+      expect(
+        body,
+        'to contain',
+        `<img class="image-attachment-img" src="http://localhost:31337/attachments/thumbnails/${att.id}.webp" alt="${att.fileName}`,
+      );
     });
   });
 
@@ -31,31 +70,37 @@ describe('BestOfDigests', () => {
 
     it(`should send daily best of email and doesn't blow up`, async () => {
       const digestDate = 'April 1st';
-      const data = { posts: [] };
+      const dailySummary = await getSummary(luna, 1);
       const user = { email: 'luna@example.com' };
 
-      await sendDailyBestOfEmail(user, data, digestDate);
+      await sendDailyBestOfEmail(user, dailySummary, digestDate);
 
       expect(capturedMail, 'to satisfy', { envelope: { to: [user.email] } });
       const parsedMail = await simpleParser(capturedMail.response);
       expect(parsedMail, 'to satisfy', {
         subject: renderEJS(config.mailer.dailyBestOfDigestMailSubject, { digestDate }),
-        html: expect.it('to contain', '<div class="posts"></div>'),
+        html: expect.it(
+          'to contain',
+          '<div class="posts"><div class="post timeline-post" data-author="luna"',
+        ),
       });
     });
 
     it(`should send weekly best of email and doesn't blow up`, async () => {
       const digestDate = 'April 1st';
-      const data = { posts: [] };
+      const weeklySummary = await getSummary(luna, 7);
       const user = { email: 'luna@example.com' };
 
-      await sendWeeklyBestOfEmail(user, data, digestDate);
+      await sendWeeklyBestOfEmail(user, weeklySummary, digestDate);
 
       expect(capturedMail, 'to satisfy', { envelope: { to: [user.email] } });
       const parsedMail = await simpleParser(capturedMail.response);
       expect(parsedMail, 'to satisfy', {
         subject: renderEJS(config.mailer.weeklyBestOfDigestMailSubject, { digestDate }),
-        html: expect.it('to contain', '<div class="posts"></div>'),
+        html: expect.it(
+          'to contain',
+          '<div class="posts"><div class="post timeline-post" data-author="luna"',
+        ),
       });
     });
   });
