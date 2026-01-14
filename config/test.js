7e9603b4d8e8e45f3e22aaaf2cd714870cae15a6
David Mzareulyan <david@hiero.ru>
2026-01-14 18:02:15 +0300

    Replace custom `nodeDirname` helper with native Node.js `dirname` and `fileURLToPath` in test config

--- config/test.js
+++ config/test.js
@@ -1,10 +1,9 @@
-import { resolve } from 'path';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 
 import stubTransport from 'nodemailer-stub-transport';
 
-import { nodeDirname } from '../app/support/node-dirname';
-
-const __dirname = nodeDirname(import.meta.url);
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default {
   port: 31337,
